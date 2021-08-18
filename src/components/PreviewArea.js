88f114412c433e4f2b9c61ef6f8efd4f7deb18ea
Peeyush Goyal <smartrohan1514@gmail.com>
2021-08-18 17:16:29 +0530

    Added Drag and Drop

--- src/components/PreviewArea.js
+++ src/components/PreviewArea.js
@@ -4,36 +4,54 @@ import { connect } from "react-redux";
 import { addCharacter, setActive } from "../redux/character/actions";
 
 function PreviewArea({ character, add_character, set_active }) {
-  function drag_start(event) {
-    console.log("Event 1 called");
-    // var style = window.getComputedStyle(event.target, null);
-    // event.dataTransfer.setData(
-    //   "text/plain",
-    //   parseInt(style.getPropertyValue("left"), 10) -
-    //     event.clientX +
-    //     "," +
-    //     (parseInt(style.getPropertyValue("top"), 10) - event.clientY)
-    // );
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+
+  let elmnt = null;
+
+  function dragMouseDown(e, id) {
+    elmnt = document.getElementById(id);
+
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
   }
-  function drag_over(event) {
-    event.preventDefault();
-    return false;
+
+  function elementDrag(e) {
+    console.log("Inside this");
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+    console.log("ElemX is ", e.clientX);
+    console.log("ElemY is ", e.clientY);
   }
-  function drop(event, i) {
-    console.log("Event 3 called ", event);
-    var offset = event.dataTransfer.getData("text/plain").split(",");
-    var dm = document.getElementById(`dragme-${i}`);
-    dm.style.left = event.clientX + parseInt(offset[0], 10) + "px";
-    dm.style.top = event.clientY + parseInt(offset[1], 10) + "px";
-    event.preventDefault();
-    return false;
+
+  function closeDragElement() {
+    console.log("Closed");
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
   }
+
   return (
     <div
       className="w-full flex-none h-full overflow-y-auto p-2"
       id="preview_area"
-      onDragStart={(e) => drag_start(e)}
-      onDragOver={(e) => drag_over(e)}
     >
       <div className="flex justify-between">
         <div className="font-bold">Preview Area</div>
@@ -48,6 +66,7 @@ function PreviewArea({ character, add_character, set_active }) {
             {character.characters.map((x, i) => {
               const first = x.id.charAt(0).toUpperCase();
               const name = first + x.id.substr(1);
+
               return (
                 <option
                   key={i}
@@ -70,16 +89,21 @@ function PreviewArea({ character, add_character, set_active }) {
       <div className="flex justify-around">
         {character.characters.map((x, i) => {
           return (
-            <div id={`dragme-${i}`} key={i}>
-              <div
-                className="hidden border-2 p-2 ml-3 mb-2 w-auto whitespace-nowrap"
-                id={x.id + "-message-box"}
-              ></div>
-              <div
-                className="hidden border-2 w-4 left-1/2 h-4 ml-3 mb-2 whitespace-nowrap"
-                id={x.id + "-message-box1"}
-              ></div>
-              <div>
+            <div
+              id={`${x.id}-${i}`}
+              key={i}
+              className="absolute"
+              onMouseDown={(e) => dragMouseDown(e, `${x.id}-${i}`)}
+            >
+              <div id={`${x.id}-div`}>
+                <div
+                  className="hidden border-2 p-2 ml-3 mb-2 w-auto whitespace-nowrap"
+                  id={x.id + "-message-box"}
+                ></div>
+                <div
+                  className="hidden border-2 w-4 left-1/2 h-4 ml-3 mb-2 whitespace-nowrap"
+                  id={x.id + "-message-box1"}
+                ></div>
                 <CatSprite charac_id={x.id} />
               </div>
             </div>
